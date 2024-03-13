f4395f6fbb7def0d0057956411fe015a74982de7
Frederic Peschanski <frederic.peschanski@lip6.fr>
2024-03-13 15:14:32 +0100

    non-deterministic events are profunctors

--- EventSystems/NonDet/Basic.lean
+++ EventSystems/NonDet/Basic.lean
@@ -14,10 +14,58 @@ def _NDEvent_fromEvent [Machine CTX M] (ev : _Event M α β) : _NDEvent M α β 
                                   m'' = m' ∧ x'' = x'
 }
 
+-- The functor instance is existential, not suprising given the relational context
+instance [Machine CTX M] : Functor (_NDEvent M γ) where
+  map f ev :=
+  {
+    guard := ev.guard
+    effect := fun m x (y, m') => ∃ z, ∃ m'', ev.effect m x (z, m'')
+                                             ∧ y = f z ∧ m' = m''
+  }
+
+instance [Machine CTX M] : LawfulFunctor (_NDEvent M γ) where
+  map_const := rfl
+  id_map ev := by simp [Functor.map]
+                  cases ev
+                  case mk evr eff =>
+                    simp
+                    funext m x (y,m')
+                    apply propext
+                    constructor
+                    · intro Hz
+                      cases Hz
+                      case _ z H =>
+                        simp at H
+                        simp [H]
+                    intro Heff
+                    simp
+                    exists y
+
+  comp_map g h ev := by simp [Functor.map]
+                        funext m x (y,m')
+                        apply propext
+                        constructor
+                        · intro Hz
+                          cases Hz
+                          case _ z H =>
+                            simp at H
+                            simp
+                            exists (g z)
+                            constructor
+                            · exists z
+                              simp [H]
+                            simp [H]
+                        simp
+                        intros z t Heff Heq₁ Heq₂
+                        exists t
+                        simp [Heff, Heq₁, Heq₂]
+
+
 -- There are two possible, distinct contravariant functors
 -- for non-deterministic events.
 
--- The first operates on the output
+-- The first operates on the output, hence it cannot be use
+-- in a profunctor context
 instance [Machine CTX M] : Contravariant (_NDEvent M γ) where
   contramap f ev :=
   {
@@ -52,4 +100,44 @@ instance [Machine CTX M] : LawfullContravariant (_CoNDEvent M γ) where
   cmap_id _ := rfl
   cmap_comp _ _ := rfl
 
--- Question : is there a covariant functor ?
+-- There is a unique possible profunctor instance
+instance [Machine CTX M] : Profunctor (_NDEvent M) where
+  dimap {α β} {γ δ} (f : β → α) (g : γ → δ) (ev : _NDEvent M α γ) : _NDEvent M β δ :=
+  let ev' := NDEvent_from_CoNDEvent (Contravariant.contramap f (CoNDEvent_from_NDEvent ev))
+  g <$> ev'
+
+instance [Machine CTX M] : LawfulProfunctor (_NDEvent M) where
+  dimap_id := by simp [Profunctor.dimap, Contravariant.contramap]
+                 exact fun {α β} => rfl
+  dimap_comp f f' g g' := by simp [Profunctor.dimap, Contravariant.contramap, Functor.map]
+                             funext ev
+                             simp
+                             funext m x (y,m')
+                             simp
+                             constructor
+                             · intro Heff
+                               cases Heff
+                               case _ u Heff =>
+                                 exists (g' u)
+                                 simp [Heff]
+                                 exists u
+                                 simp [Heff]
+                             intro Heff
+                             cases Heff
+                             case _ t Heff =>
+                               simp at Heff
+                               cases Heff
+                               case _ Heff Hy =>
+                                 cases Heff
+                                 case _ u Heff =>
+                                   exists u
+                                   simp [Heff, Hy]
+
+instance [Machine CTX M] : StrongProfunctor (_NDEvent M) where
+  first' {α β γ} (ev : _NDEvent M α β): _NDEvent M (α × γ) (β × γ) :=
+    {
+      guard := fun m (x, _) => ev.guard m x
+      effect := fun m (x,u) ((y, v), m') => v = u ∧ ev.effect m x (y, m')
+    }
+
+instance [Machine CTX M] : LawfulStrongProfunctor (_NDEvent M) where
