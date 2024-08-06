1a62dda76323f8a47c61da4b8e13fdea5b4496a3
Frederic Peschanski <frederic.peschanski@lip6.fr>
2024-08-06 17:59:16 +0200

    concrete event updated for strong refinement

--- LeanMachines/Refinement/Strong/NonDet/Concrete.lean
+++ LeanMachines/Refinement/Strong/NonDet/Concrete.lean
@@ -6,16 +6,51 @@ open FRefinement
 open SRefinement
 
 @[simp]
-def newConcreteSRNDEvent [Preorder v] [WellFoundedLT v] [Machine ACTX AM] [Machine CTX M] [SRefinement AM M]
-  (ev : ConcreteFRNDEventSpec v AM M α β) : ConvergentRNDEvent v AM M α β :=
+def newConcreteSRNDEvent [Machine ACTX AM] [Machine CTX M] [SRefinement AM M]
+  (ev : ConcreteFRNDEventSpec AM M α β) : OrdinaryRNDEvent AM M α β :=
   newConcreteFRNDEvent ev
 
 @[simp]
-def newConcreteSRNDEvent' [Preorder v] [WellFoundedLT v] [Machine ACTX AM] [Machine CTX M] [SRefinement AM M]
-  (ev : ConcreteFRNDEventSpec' v AM M α) : ConvergentRNDEvent v AM M α Unit :=
+def newConcreteSRNDEvent' [Machine ACTX AM] [Machine CTX M] [SRefinement AM M]
+  (ev : ConcreteFRNDEventSpec' AM M α) : OrdinaryRNDEvent AM M α Unit :=
   newConcreteSRNDEvent ev.toConcreteFRNDEventSpec
 
 @[simp]
-def newConcreteSRNDEvent'' [Preorder v] [WellFoundedLT v] [Machine ACTX AM] [Machine CTX M] [SRefinement AM M]
-  (ev : ConcreteFRNDEventSpec'' v AM M) : ConvergentRNDEvent v AM M Unit Unit :=
+def newConcreteSRNDEvent'' [Machine ACTX AM] [Machine CTX M] [SRefinement AM M]
+  (ev : ConcreteFRNDEventSpec'' AM M) : OrdinaryRNDEvent AM M Unit Unit :=
   newConcreteSRNDEvent ev.toConcreteFRNDEventSpec
+
+/- Anticipated concrete events -/
+
+@[simp]
+def newConcreteAnticipatedSRNDEvent [Preorder v] [WellFoundedLT v] [Machine ACTX AM] [Machine CTX M] [SRefinement AM M]
+  (ev : ConcreteAnticipatedFRNDEventSpec v AM M α β) : AnticipatedRNDEvent v AM M α β :=
+  newConcreteAnticipatedFRNDEvent ev
+
+@[simp]
+def newConcreteAnticipatedSRNDEvent' [Preorder v] [WellFoundedLT v] [Machine ACTX AM] [Machine CTX M] [SRefinement AM M]
+  (ev : ConcreteAnticipatedFRNDEventSpec' v AM M α) : AnticipatedRNDEvent v AM M α Unit :=
+  newConcreteAnticipatedSRNDEvent ev.toConcreteAnticipatedFRNDEventSpec
+
+@[simp]
+def newConcreteAnticipatedSRNDEvent'' [Preorder v] [WellFoundedLT v] [Machine ACTX AM] [Machine CTX M] [SRefinement AM M]
+  (ev : ConcreteAnticipatedFRNDEventSpec'' v AM M) : AnticipatedRNDEvent v AM M Unit Unit :=
+  newConcreteAnticipatedSRNDEvent ev.toConcreteAnticipatedFRNDEventSpec
+
+
+/- Convergent concrete events -/
+
+@[simp]
+def newConcreteConvergentSRNDEvent [Preorder v] [WellFoundedLT v] [Machine ACTX AM] [Machine CTX M] [SRefinement AM M]
+  (ev : ConcreteConvergentFRNDEventSpec v AM M α β) : ConvergentRNDEvent v AM M α β :=
+  newConcreteConvergentFRNDEvent ev
+
+@[simp]
+def newConcreteConvergentSRNDEvent' [Preorder v] [WellFoundedLT v] [Machine ACTX AM] [Machine CTX M] [SRefinement AM M]
+  (ev : ConcreteConvergentFRNDEventSpec' v AM M α) : ConvergentRNDEvent v AM M α Unit :=
+  newConcreteConvergentSRNDEvent ev.toConcreteConvergentFRNDEventSpec
+
+@[simp]
+def newConcreteConvergentSRNDEvent'' [Preorder v] [WellFoundedLT v] [Machine ACTX AM] [Machine CTX M] [SRefinement AM M]
+  (ev : ConcreteConvergentFRNDEventSpec'' v AM M) : ConvergentRNDEvent v AM M Unit Unit :=
+  newConcreteConvergentSRNDEvent ev.toConcreteConvergentFRNDEventSpec
