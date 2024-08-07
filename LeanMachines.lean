4a354490d235d98c559d81efeb0820dcf05471f1
Frederic Peschanski <frederic.peschanski@lip6.fr>
2024-08-07 15:40:02 +0200

    better build imports

--- LeanMachines.lean
+++ LeanMachines.lean
@@ -1,3 +1,35 @@
 -- This module serves as the root of the `EventSystems` library.
 -- Import modules here that should be built as part of the library.
-import «EventSystems».Basic
+
+-- Abstract machines
+import «LeanMachines».Event.Convergent
+import «LeanMachines».NonDet.Convergent
+
+-- Relational refinement
+import «LeanMachines».Refinement.Relational.Abstract
+import «LeanMachines».Refinement.Relational.Concrete
+import «LeanMachines».Refinement.Relational.NonDet.Abstract
+import «LeanMachines».Refinement.Relational.NonDet.Concrete
+import «LeanMachines».Refinement.Relational.NonDet.Det.Convergent
+
+-- Functional refinement
+import «LeanMachines».Refinement.Functional.Abstract
+import «LeanMachines».Refinement.Functional.Concrete
+import «LeanMachines».Refinement.Functional.NonDet.Abstract
+import «LeanMachines».Refinement.Functional.NonDet.Concrete
+import «LeanMachines».Refinement.Functional.NonDet.Det.Convergent
+
+-- Strong (superposition) refinement
+import «LeanMachines».Refinement.Strong.Abstract
+import «LeanMachines».Refinement.Strong.Concrete
+import «LeanMachines».Refinement.Strong.NonDet.Abstract
+import «LeanMachines».Refinement.Strong.NonDet.Concrete
+import «LeanMachines».Refinement.Strong.NonDet.Det.Convergent
+
+-- Versioning
+
+def LeanMachines.VERSION_MAJ : Nat := 0
+def LeanMachines.VERSION_MIN  : Nat := 2
+def LeanMachines.VERSION_REV  : Nat := 0
+
+def LeanMachines.VERSION_STS : String := "beta"
--- Main.lean
+++ Main.lean
@@ -1,10 +1,6 @@
-import «LeanMachines».Event.Basic
-import «LeanMachines».Event.Ordinary
-import «LeanMachines».Event.Convergent
-import «LeanMachines».NonDet.Basic
-import «LeanMachines».NonDet.Convergent
-import «LeanMachines».Refinement.Relational.Basic
-import «LeanMachines».Refinement.Relational.Convergent
+
+
+import «LeanMachines»
 
 def main : IO Unit :=
   IO.println s!"Hello, {hello}!"
