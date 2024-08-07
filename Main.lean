31361b1a1b08ea506aeaf2d87b17515787175edd
Fredokun <pesch@AlphaCentauri.>
2024-08-07 15:26:13 +0200

    examples in another repository

--- Main.lean
+++ Main.lean
@@ -6,13 +6,5 @@ import «LeanMachines».NonDet.Convergent
 import «LeanMachines».Refinement.Relational.Basic
 import «LeanMachines».Refinement.Relational.Convergent
 
-import «Examples».EventB.Bridge.Bridge2
-
-import «Examples».EventB.Courses.M2
-
-import «Examples».Buffer.Buffer2
-
-import «Examples».VDM.AlarmSystem.AlarmSystem2
-
 def main : IO Unit :=
   IO.println s!"Hello, {hello}!"
