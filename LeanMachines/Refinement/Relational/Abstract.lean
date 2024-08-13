3558857dd97c4c159a19148ef757cb2eab8e79da
Frederic Peschanski <frederic.peschanski@lip6.fr>
2024-08-13 11:23:36 +0200

    abstract event (relational): documented

--- LeanMachines/Refinement/Relational/Abstract.lean
+++ LeanMachines/Refinement/Relational/Abstract.lean
@@ -1,51 +1,108 @@
 
-/-
-  Reuse of abstract events
--/
 
 import LeanMachines.Refinement.Relational.Basic
 import LeanMachines.Refinement.Relational.Convergent
 
+/-!
+
+# Reusing abstract (deterministic, ordinary) events
+
+This module establish generic principles for the
+reuse of abstract events at the concrete level
+ in case of a relational refinement.
+
+This is a non-trivial issue in most cases, in that
+there is no reason in general for an abstract event - working on the
+abstract machine state - to be applicable on the
+state of the concrete machine.
+
+The reuse of abstract events should be easy (or a least easier) in
+case of *superposition* refinement. This use case is covered by
+the strong refinement principles (cf. `Refinement.Strong` modules).
+
+In the proposed model, an abstract event specification must:
+
+ - specify the abstract event to be reused
+
+ - explain how the concrete state can be reflected at the abstract level
+   (which is named, a `lift` operation)
+
+ - explain how an abstract change is reflected at the concrete level
+   (which is named, an `unlift` operation)
+
+There are also two required proof obligations :
+
+1. that the `lift` operation respects the `refine` (or glue) invariant
+   connecting the concrete and abstract machines
+
+2. a *functionality* argument for the refine (glue) invariant.
+
+From this, we are able to prove that the abstract event can be
+*safely* reused at the concrete level with two safety proof
+obligations wrt. the `lift` and `unlift` operations.
+
+**Important**: the `lift` function operates globally at the machine level.
+Hence, a generally better approach is to adopt (at least) a functional refinement
+approach,  cf. the `Refinement.Functional` modules.
+
+However, the constructions below are required for the soundness
+of the functional and strong refinement principles.
+
+-/
+
+
 open Refinement
 
-/-
- Q : is there a better way to ease the definition of abstract events ? -/
+/-!
+## Transitional abstract events
+-/
 
+/-- Reuse of abstract events: the `lift ` part of machine-level requirements. -/
 structure __AbstractREventSpec (AM) [Machine ACTX AM]
                                (M) [Machine CTX M]
                                [Refinement AM M] where
-
+  /-- The construction of an abstract state from the concrete state `m`. -/
   lift (m: M) : AM
 
+  /-- Proof obligation: the `lift` operation is safe wrt. the `refine` invariant.  -/
   lift_ref (m : M):
     Machine.invariant m → refine (lift m) m
 
+  /-- Proof obligation: functionality constraint for the `refine` invariant. -/
   refine_uniq (am am' : AM) (m : M):
     Machine.invariant m
     → refine am m → refine am' m
     → am = am'
 
 
+/-- Reuse of abstract events: the `unlift ` part of machine-level requirements. -/
 structure _AbstractREventSpec (AM) [Machine ACTX AM]
                               (M) [Machine CTX M]
                               [Refinement AM M] (α)
 
   extends __AbstractREventSpec AM M  where
 
+  /-- The reconstruction of a concrete state from an abstract state-change
+  performed by the abstract event with input `x`. -/
   unlift (am am' : AM) (m : M) (x : α): M
 
+/-- The specification of the reuse of an `abstract` event, with the
+`lift` and `unlift` operations, the associated proof obligations,
+ and the further two event-level proof obligations. -/
 structure AbstractREventSpec (AM) [Machine ACTX AM]
                              (M) [Machine CTX M]
                             [Refinement AM M]
   {α β} (abstract : OrdinaryEvent AM α β)
           extends _AbstractREventSpec AM M α where
 
+  /-- Proof obligation: lifting, then unlifting is safe wrt. the `refine` invariant. -/
   step_ref (m : M) (x : α):
     Machine.invariant m
     → abstract.guard (lift m) x
     → let (_, am') := abstract.action (lift m) x
       refine am' (unlift (lift m) am' m x)
 
+  /-- Proof obligation: invariant preservation of the abstract event. -/
   step_safe (m : M) (x : α):
     Machine.invariant m
     → abstract.guard (lift m) x
@@ -53,6 +110,9 @@ structure AbstractREventSpec (AM) [Machine ACTX AM]
       Machine.invariant am' -- redundant but useful
       → Machine.invariant (unlift (lift m) am' m x)
 
+/-- The construction of a reused abstract event (ordinary case).
+Parameter `abs` is the event to reuse  (event of abstract machine)
+Parameter `ev` is the reuse specification (cf. `AbstractREventSpec`). -/
 @[simp]
 def newAbstractREvent [Machine ACTX AM] [Machine CTX M] [instR:Refinement AM M]
   (abs : OrdinaryEvent AM α β) (ev : AbstractREventSpec AM M abs) : OrdinaryREvent AM M α β :=
@@ -94,6 +154,7 @@ def newAbstractREvent [Machine ACTX AM] [Machine CTX M] [instR:Refinement AM M]
     }
   }
 
+/-- Variant of `newAbstractREvent` with implicit `Unit` output type -/
 @[simp]
 def newAbstractREvent' [Machine ACTX AM] [Machine CTX M] [Refinement AM M]
   (abs : OrdinaryEvent AM α Unit) (ev : AbstractREventSpec AM M abs) : OrdinaryREvent AM M α Unit :=
@@ -107,6 +168,7 @@ structure _AbstractREventSpec'' (AM) [Machine ACTX AM]
 
   unlift (am am' : AM) (m : M): M
 
+/-- Variant of `AbstractREventSpec` with implicit `Unit` input and output types -/
 structure AbstractREventSpec'' (AM) [Machine ACTX AM]
                                (M) [Machine CTX M]
                                [Refinement AM M]
@@ -137,22 +199,32 @@ def AbstractREventSpec''.toAbstractREventSpec [Machine ACTX AM] [Machine CTX M] 
   }
 
 
+/-- Variant of `newAbstractREvent` with implicit `Unit` input and output types -/
 @[simp]
 def newAbstractREvent'' [Machine ACTX AM] [Machine CTX M] [Refinement AM M]
   (abs : OrdinaryEvent AM Unit Unit) (ev : AbstractREventSpec'' AM M abs) : OrdinaryREvent AM M Unit Unit :=
   newAbstractREvent abs ev.toAbstractREventSpec
 
+/-!
+## Initialization abstract events
+-/
+
+/-- The specification of the reuse of an `abstract` initialization event,
+with the `lift` and `unlift` operations, the associated proof obligations,
+ and the further two event-level proof obligations. -/
 structure AbstractInitREventSpec (AM) [Machine ACTX AM]
                                  (M) [Machine CTX M]
                                  [Refinement AM M]
   {α β} (abstract : InitEvent AM α β)
           extends _AbstractREventSpec AM M α where
 
+  /-- Proof obligation: lifting, then unlifting is safe wrt. the `refine` invariant. -/
   step_ref (x : α):
     abstract.guard x
     → let (_, am') := abstract.init x
       refine am' (unlift Machine.reset am' Machine.reset x)
 
+  /-- Proof obligation: invariant preservation of the abstract event. -/
   step_safe (x : α):
     abstract.guard x
     → let (_, am') := abstract.init x
@@ -168,6 +240,9 @@ def AbstractInitREventSpec.to_InitEvent [Machine ACTX AM] [Machine CTX M] [Refin
                      (y, ev.unlift Machine.reset am' Machine.reset x)
   }
 
+/-- The construction of a reused abstract initialization event.
+Parameter `abs` is the event to reuse  (event of abstract machine)
+Parameter `ev` is the reuse specification (cf. `AbstractInitREventSpec`). -/
 @[simp]
 def newAbstractInitREvent [Machine ACTX AM] [Machine CTX M] [instR:Refinement AM M]
   (abs : InitEvent AM α β) (ev : AbstractInitREventSpec AM M abs) : InitREvent AM M α β :=
@@ -191,11 +266,13 @@ def newAbstractInitREvent [Machine ACTX AM] [Machine CTX M] [instR:Refinement AM
     }
   }
 
+/-- Variant of `newAbstractInitREvent` with implicit `Unit` output type -/
 @[simp]
 def newAbstractInitREvent' [Machine ACTX AM] [Machine CTX M] [Refinement AM M]
   (abs : InitEvent AM α Unit) (ev : AbstractInitREventSpec AM M abs) : InitREvent AM M α Unit :=
   newAbstractInitREvent abs ev
 
+/-- Variant of `AbstractInitREventSpec` with implicit `Unit` input and output types -/
 structure AbstractInitREventSpec'' (AM) [Machine ACTX AM]
                                  (M) [Machine CTX M]
                                  [Refinement AM M]
@@ -223,11 +300,19 @@ def AbstractInitREventSpec''.toAbstractInitREventSpec [Machine ACTX AM] [Machine
     step_safe := fun () => ev.step_safe
   }
 
+/-- Variant of `newAbstractREvent` with implicit `Unit` input and output types -/
 @[simp]
 def newAbstractRInitEvent'' [Machine ACTX AM] [Machine CTX M] [Refinement AM M]
   (abs : InitEvent AM Unit Unit) (ev : AbstractInitREventSpec'' AM M abs) : InitREvent AM M Unit Unit :=
   newAbstractInitREvent abs ev.toAbstractInitREventSpec
 
+/-!
+## Anticipated abstract events
+-/
+
+/-- Reuse of abstract anticipated event.
+The requirements are thow of `AbstractREventSpec` with a demonstration
+that the abstract variant is preserved when reflected at the concrete level. -/
 structure AbstractAnticipatedREventSpec
               (v) [Preorder v]
               (AM) [Machine ACTX AM]
@@ -236,6 +321,7 @@ structure AbstractAnticipatedREventSpec
   {α β} (abstract : AnticipatedEvent v AM α β)
           extends AbstractREventSpec AM M abstract.toOrdinaryEvent where
 
+  /-- Proof obligation: the abstract variant is preserved at the concrete level. -/
   step_variant (m : M) (x : α):
     Machine.invariant m
     → abstract.guard (lift m) x
@@ -244,6 +330,9 @@ structure AbstractAnticipatedREventSpec
       → abstract.po.variant (lift (unlift (lift m) am' m x))
       = abstract.po.variant am'
 
+/-- The construction of a reused abstract event (anticipated case).
+Parameter `abs` is the event to reuse  (event of abstract machine)
+Parameter `ev` is the reuse specification (cf. `AbstractREventSpec`). -/
 @[simp]
 def newAbstractAnticipatedREvent [Preorder v]
                                  [Machine ACTX AM]
@@ -277,6 +366,7 @@ def newAbstractAnticipatedREvent [Preorder v]
     }
   }
 
+/-- Variant of `newAbstractAnticipatedREvent` with implicit `Unit` output type -/
 @[simp]
 def newAbstractAnticipatedREvent' [Preorder v]
                                   [Machine ACTX AM]
@@ -285,6 +375,7 @@ def newAbstractAnticipatedREvent' [Preorder v]
   (abs : AnticipatedEvent v AM α Unit) (ev : AbstractAnticipatedREventSpec v AM M abs) : AnticipatedREvent v AM M α Unit :=
   newAbstractAnticipatedREvent abs ev
 
+/-- Variant of `AbstractAnticipatedREventSpec` with implicit `Unit` input and output types -/
 structure AbstractAnticipatedREventSpec''
               (v) [Preorder v]
               (AM) [Machine ACTX AM]
@@ -312,6 +403,7 @@ def AbstractAnticipatedREventSpec''.toAbstractAnticipatedREventSpec [Preorder v]
     step_variant := fun m _ => ev.step_variant m
   }
 
+/-- Variant of `newAbstractAnticipatedREvent` with implicit `Unit` output type -/
 @[simp]
 def newAbstractAnticipatedREvent'' [Preorder v]
                                   [Machine ACTX AM]
@@ -320,6 +412,9 @@ def newAbstractAnticipatedREvent'' [Preorder v]
   (abs : AnticipatedEvent v AM Unit Unit) (ev : AbstractAnticipatedREventSpec'' v AM M abs) : AnticipatedREvent v AM M Unit Unit :=
   newAbstractAnticipatedREvent abs ev.toAbstractAnticipatedREventSpec
 
+/-- The construction of a reused abstract event (convergent case).
+Parameter `abs` is the event to reuse  (event of abstract machine)
+Parameter `ev` is the reuse specification (cf. `AbstractREventSpec`). -/
 @[simp]
 def newAbstractConvergentREvent  [Preorder v] [WellFoundedLT v]
                                  [Machine ACTX AM]
@@ -353,6 +448,7 @@ def newAbstractConvergentREvent  [Preorder v] [WellFoundedLT v]
     }
   }
 
+/-- Variant of `newAbstractConvergentREvent` with implicit `Unit` output type -/
 @[simp]
 def newAbstractConvergentREvent'  [Preorder v] [WellFoundedLT v]
                                   [Machine ACTX AM]
@@ -361,6 +457,7 @@ def newAbstractConvergentREvent'  [Preorder v] [WellFoundedLT v]
   (abs : ConvergentEvent v AM α Unit) (ev : AbstractAnticipatedREventSpec v AM M abs.toAnticipatedEvent) : ConvergentREvent v AM M α Unit :=
   newAbstractConvergentREvent abs ev
 
+/-- Variant of `newAbstractConvergentREvent` with implicit `Unit` input and output types -/
 @[simp]
 def newAbstractConvergentREvent''  [Preorder v] [WellFoundedLT v]
                                    [Machine ACTX AM]
