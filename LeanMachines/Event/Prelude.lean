07ff196c2b61f7950c213a65dba84b36a08a5498
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2024-08-21 12:15:41 +0200

    Update Prelude.lean

--- LeanMachines/Event/Prelude.lean
+++ LeanMachines/Event/Prelude.lean
@@ -1,31 +1,19 @@
 
 theorem And_assoc₁ (P Q R : Prop):
-  (P ∧ Q) ∧ R → P ∧ (Q ∧ R) :=
-by
-  intro H
-  simp [H]
+  (P ∧ Q) ∧ R → P ∧ (Q ∧ R) := fun H ↦ and_assoc.mp H
 
 theorem And_assoc₂ (P Q R : Prop):
-  P ∧ (Q ∧ R) → (P ∧ Q) ∧ R :=
-by
-  intro H
-  simp [H]
+  P ∧ (Q ∧ R) → (P ∧ Q) ∧ R := fun H ↦ and_assoc.mpr H
 
 theorem And_assoc (P Q R : Prop):
   (P ∧ Q) ∧ R ↔ P ∧ (Q ∧ R) := ⟨And_assoc₁ P Q R, And_assoc₂ P Q R⟩
 
 theorem And_eq_assoc (P Q R : Prop):
-  ((P ∧ Q) ∧ R) = (P ∧ (Q ∧ R)) :=
-by
-  apply propext
-  apply And_assoc
+  ((P ∧ Q) ∧ R) = (P ∧ (Q ∧ R)) := propext (And_assoc _ _ _)
 
 theorem Eq_fun (e₁ : α → β) (e₂ : α → β):
   (fun x : α => e₁ x) = (fun y : α => e₂ y)
-  → e₁ = e₂ :=
-by
-  intro Heq
-  exact Heq
+  → e₁ = e₂ := fun Heq ↦ Heq
 
 inductive Either (α β : Type) where
 | left : α → Either α β
