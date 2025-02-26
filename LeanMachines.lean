9b5b41ba66a0aaf2a3362a7b6b58c1e01101befd
DanaelCarbonneau <danaele.carbonneau@etu.sorbonne-universite.fr>
2025-02-26 12:11:58 +0100

    imports

--- LeanMachines.lean
+++ LeanMachines.lean
@@ -2,29 +2,29 @@
 -- Import modules here that should be built as part of the library.
 
 -- Abstract machines
-import «LeanMachines».Event.Convergent
-import «LeanMachines».NonDet.Convergent
+-- import «LeanMachines».Event.Convergent
+-- import «LeanMachines».NonDet.Convergent
 
 -- Relational refinement
-import «LeanMachines».Refinement.Relational.Abstract
-import «LeanMachines».Refinement.Relational.Concrete
-import «LeanMachines».Refinement.Relational.NonDet.Abstract
-import «LeanMachines».Refinement.Relational.NonDet.Concrete
-import «LeanMachines».Refinement.Relational.NonDet.Det.Convergent
+-- import «LeanMachines».Refinement.Relational.Abstract
+-- import «LeanMachines».Refinement.Relational.Concrete
+-- import «LeanMachines».Refinement.Relational.NonDet.Abstract
+-- import «LeanMachines».Refinement.Relational.NonDet.Concrete
+-- import «LeanMachines».Refinement.Relational.NonDet.Det.Convergent
 
 -- Functional refinement
-import «LeanMachines».Refinement.Functional.Abstract
-import «LeanMachines».Refinement.Functional.Concrete
-import «LeanMachines».Refinement.Functional.NonDet.Abstract
-import «LeanMachines».Refinement.Functional.NonDet.Concrete
-import «LeanMachines».Refinement.Functional.NonDet.Det.Convergent
+-- import «LeanMachines».Refinement.Functional.Abstract
+-- import «LeanMachines».Refinement.Functional.Concrete
+-- import «LeanMachines».Refinement.Functional.NonDet.Abstract
+-- import «LeanMachines».Refinement.Functional.NonDet.Concrete
+-- import «LeanMachines».Refinement.Functional.NonDet.Det.Convergent
 
 -- Strong (superposition) refinement
-import «LeanMachines».Refinement.Strong.Abstract
-import «LeanMachines».Refinement.Strong.Concrete
-import «LeanMachines».Refinement.Strong.NonDet.Abstract
-import «LeanMachines».Refinement.Strong.NonDet.Concrete
-import «LeanMachines».Refinement.Strong.NonDet.Det.Convergent
+-- import «LeanMachines».Refinement.Strong.Abstract
+-- import «LeanMachines».Refinement.Strong.Concrete
+-- import «LeanMachines».Refinement.Strong.NonDet.Abstract
+-- import «LeanMachines».Refinement.Strong.NonDet.Concrete
+-- import «LeanMachines».Refinement.Strong.NonDet.Det.Convergent
 
 -- Versioning
 
